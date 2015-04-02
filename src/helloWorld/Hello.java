d6f7c0784d0b4a9a9a10e329988875ad21c2549e
newhhz <newhhz@163.com>
2015-04-02 15:26:39 +0800

    s4

--- src/helloWorld/Hello.java
+++ src/helloWorld/Hello.java
@@ -5,8 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		
-		//¶À´´µÄ
+		//test2222
 	}
         //to do 
 }
